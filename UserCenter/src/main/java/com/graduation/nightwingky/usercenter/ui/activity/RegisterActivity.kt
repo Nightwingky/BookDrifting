a105ae36b1ea524004a73c61f77dc98f0244e84d
nightwingky <qky1024@163.com>
2018-05-03 15:34:11 +0800

    创建Module　UserCenter可动态切换

---
+++ UserCenter/src/main/java/com/graduation/nightwingky/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.graduation.nightwingky.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.graduation.nightwingky.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
